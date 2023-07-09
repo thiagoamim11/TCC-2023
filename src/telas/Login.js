6ece936e77cf21476b9f7b13b68776f0ba952d42
Thiago Amim <thiagoamim11@gmail.com>
2023-07-09 01:40:53 -0300

    Tela de logar quase pronta

--- src/telas/Login.js
+++ src/telas/Login.js
@@ -1,54 +1,60 @@
-import React,{useState} from "react";
-import { StyleSheet,Text, TouchableOpacity, View,Button, SafeAreaView, TextInput } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View, Button, SafeAreaView, TextInput } from "react-native";
 import { Logo } from "../componentes/Logo";
 
 
-export default function Login ({navigation}) {
- return(
-  
+export default function Login({ navigation }) {
+  return (
+
 
 
 
     <SafeAreaView style={estilos.container}>
-      <Logo/>
+      <Logo />
 
       <View>
-        <Text style={estilos.texto}>Login</Text>
+        <Text style={estilos.texto}>Login*</Text>
         <TextInput
-        style={estilos.input} 
-        placeholder="Digite seu email" 
-        backgroundColor='white' 
+          style={estilos.input}
+          placeholder="Digite seu email"
+          backgroundColor='white'
         />
 
-        <Text>Senha</Text>
+        <Text style={estilos.texto}>Senha*</Text>
         <TextInput
-        style={estilos.input} 
-        placeholder="Digite a senha" 
-        backgroundColor='white' 
+          style={estilos.input}
+          placeholder="Digite a senha"
+          backgroundColor='white'
         />
-        
+
       </View>
 
-   <TouchableOpacity style={estilos} onPress={() => navigation.navigate('Cadastro')}>
-    <Text>BOTAO </Text>
-   </TouchableOpacity>
+      <View style={estilos.containerBotao}>
+        <TouchableOpacity style={estilos} onPress={() => navigation.navigate('Cadastro')}>
+          <Text>Logar</Text>
+        </TouchableOpacity>
 
-   
+        <TouchableOpacity style={estilos} onPress={() => navigation.navigate('Cadastro')}>
+          <Text>Cadastra-se</Text>
+        </TouchableOpacity>
+      </View>
 
-   </SafeAreaView>
-    
-);
+
+    </SafeAreaView>
+
+  );
 }
 
-const estilos = StyleSheet.create ({
-    input: {
+const estilos = StyleSheet.create({
+  input: {
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10,
-    borderRadius:20,
-    alignContent:'center',
+    borderRadius: 20,
+    alignContent: 'center',
     textAlign: 'center',
+
   },
 
   container: {
@@ -57,9 +63,25 @@ const estilos = StyleSheet.create ({
   },
 
   texto: {
-    fontWeight: 'bold',
+    fontSize: 15,
+    paddingLeft: 20,
+    marginTop: 15,
+
+  },
+
+  containerBotao: {
+    justifyContent: 'space-around',
+    flexDirection: 'row-reverse',
     
   },
+
+  botao: {
+    alignItems: 'center',
+    backgroundColor: 'black',
+    padding: 10,
+    borderRadius:20,
+  }
+
 
 })
 
