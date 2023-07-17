ea961d5ea4e0f0b56fc93e1320adc14aa500be3f
thiagooterolima <72893583+thiagooterolima@users.noreply.github.com>
2023-07-17 13:09:19 -0300

    Adicionando Informações das empresas

--- src/telas/TelaDoUsuario.js
+++ src/telas/TelaDoUsuario.js
@@ -9,8 +9,10 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 
 
 export const TelaDoUsuario = ({navigation}) => {
-    const phoneNumber1 = '43-99280-8488';
-    const phoneNumber2 = '43-9987-8212';
+    const phoneNumber1 = '32 98875-5200';
+    const phoneNumber2 = '31 99422-4365';
+    const phoneNumber3 = '00 00000-0000';
+
 
     const openDialPad1 = () => {
     Linking.openURL(`tel:${phoneNumber1}`);
@@ -20,14 +22,23 @@ export const TelaDoUsuario = ({navigation}) => {
     Linking.openURL(`tel:${phoneNumber2}`); 
     };
 
+    const openDialPad3 = () => {
+    Linking.openURL(`tel:${phoneNumber3}`); 
+    };
+
     const Data = {
+
         id : '1',
         nome : 'Confiança',
         localidade :'Curitiba - PR',
 
         id : '2',
-        nome1 :'Serviço Feito',
-        localidade1 :'Belo Horizonte'
+        nome1 :'Urbana',
+        localidade1 :'Belo Horizonte',
+
+        id : '3',
+        nome2 :'VAZIO',
+        localidade2 :'VAZIO'
     }
 
 
@@ -37,7 +48,9 @@ export const TelaDoUsuario = ({navigation}) => {
         <ScrollView style={estilos.gambiarra}>
 
             <Logo/>
+
             <SafeAreaView style={estilos.container}>
+
                 <View style={estilos.containerInput}>
                 <TextInput 
                 style={estilos.input}
@@ -45,20 +58,21 @@ export const TelaDoUsuario = ({navigation}) => {
                 />
                 </View>
 
+
                 <View style={estilos.container2}>
+
                 <Text style={estilos.input}>{Data.nome} </Text>
+
                 <Text style={estilos.input}>{Data.localidade}</Text>  
 
                <View style={estilos.container_button}>
-               <FontAwesome name="phone" size={25} color={'#000000'} onPress={openDialPad2} style={{ marginHorizontal: 20, marginVertical:5 }}/>
-               <FontAwesome name="facebook" size={25} color={'#000000'} onPress={() => Linking.openURL('https://www.example.com')} style={{ marginHorizontal: 20, marginVertical:5 }}/>
-               <Ionicons name="location" size={25} color={'#000000'}  onPress={() => Linking.openURL('https://www.google.com/maps/place//data=!4m2!3m1!1s0x94dcfbac4e0a5631:0xeb19541f87038e0c')} style={{ marginHorizontal: 20, marginVertical:5 }}/>
-               <FontAwesome name="instagram" size={25} color={'#000000'} onPress={() => Linking.openURL('https://www.example.com')} style={{ marginHorizontal: 20, marginVertical:5 }}/>  
-
-                </View>
-               
-
-                <TextInput
+                <FontAwesome name="phone" size={25} color={'#000000'} onPress={openDialPad1} style={{ marginHorizontal: 20, marginVertical:5 }}/>
+                <FontAwesome name="facebook" size={25} color={'#000000'} onPress={() => Linking.openURL('https://www.example.com')} style={{ marginHorizontal: 20, marginVertical:5 }}/>
+                <Ionicons name="location" size={25} color={'#000000'}  onPress={() => Linking.openURL('https://www.google.com.br/maps/place/R.+Bar%C3%A3o+de+Santo+%C3%82ngelo,+451+-+Xaxim,+Curitiba+-+PR,+81810-140/@-25.5098228,-49.2832227,17z/data=!3m1!4b1!4m6!3m5!1s0x94dcfb5e5ecd69f7:0x16df3eb165b80bab!8m2!3d-25.5098277!4d-49.2806478!16s%2Fg%2F11v0_53h_b?entry=ttu')} style={{ marginHorizontal: 20, marginVertical:5 }}/>
+                <FontAwesome name="instagram" size={25} color={'#000000'} onPress={() => Linking.openURL('https://www.example.com')} style={{ marginHorizontal: 20, marginVertical:5 }}/>  
+               </View>
+            
+               <TextInput
                 style={estilos.input2}
                 
                 />
@@ -72,12 +86,14 @@ export const TelaDoUsuario = ({navigation}) => {
                 <Text style={estilos.input2}>{Data.localidade1}</Text>
 
                 
-                
+               <View style={estilos.container_button}>
+                <FontAwesome name="phone" size={25} color={'#000000'} onPress={openDialPad2} style={{ marginHorizontal: 20, marginVertical:5 }}/>
+                <FontAwesome name="facebook" size={25} color={'#000000'} onPress={() => Linking.openURL('https://www.example.com')} style={{ marginHorizontal: 20, marginVertical:5 }}/>
+                <Ionicons name="location" size={25} color={'#000000'}  onPress={() => Linking.openURL('https://www.google.com.br/maps/place/R.+Frei+Concei%C3%A7%C3%A3o+Veloso,+86+-+Alto+dos+Pinheiros,+Belo+Horizonte+-+MG,+30530-300/@-19.9301122,-43.9994699,17z/data=!3m1!4b1!4m6!3m5!1s0xa696f5f63a654b:0x29fd2fbe1814f33e!8m2!3d-19.9301173!4d-43.996895!16s%2Fg%2F11c218_hqp?entry=ttu')} style={{ marginHorizontal: 20, marginVertical:5 }}/>
+                <FontAwesome name="instagram" size={25} color={'#000000'} onPress={() => Linking.openURL('https://www.example.com')} style={{ marginHorizontal: 20, marginVertical:5 }}/>  
+                </View> 
 
-                <TextInput
-                style={estilos.input2}
-                
-                />
+               
 
                 <TextInput
                 style={estilos.input2}
@@ -85,6 +101,41 @@ export const TelaDoUsuario = ({navigation}) => {
                 />
 
                 </View>
+
+                <View style={estilos.container2}>
+
+                <Text style={estilos.input2}>{Data.nome2}</Text>
+                <Text style={estilos.input2}>{Data.localidade2}</Text>
+
+                <View style={estilos.container_button}>
+                <FontAwesome name="phone" size={25} color={'#000000'} onPress={openDialPad3} style={{ marginHorizontal: 20, marginVertical:5 }}/>
+                <FontAwesome name="facebook" size={25} color={'#000000'} onPress={() => Linking.openURL('https://www.example.com')} style={{ marginHorizontal: 20, marginVertical:5 }}/>
+                <Ionicons name="location" size={25} color={'#000000'}  onPress={() => Linking.openURL('https://www.example.com')} style={{ marginHorizontal: 20, marginVertical:5 }}/>
+                <FontAwesome name="instagram" size={25} color={'#000000'} onPress={() => Linking.openURL('https://www.example.com')} style={{ marginHorizontal: 20, marginVertical:5 }}/>  
+                </View> 
+
+                <TextInput
+                style={estilos.input2}
+                
+                />
+
+
+
+
+                </View>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
                 <View style={estilos.posicao_button}>
                 <TouchableOpacity style={estilos.button} onPress={() => navigation.navigate('Login')}  >
@@ -136,8 +187,7 @@ const estilos = StyleSheet.create({
     container_button: {
         height: 40,
         margin: 12,
-        borderWidth: 1,
-        
+        borderWidth: 1,  
         borderRadius: 20,
         alignContent: 'center',
         textAlign: 'center',
@@ -145,9 +195,7 @@ const estilos = StyleSheet.create({
         width: '80%',
         borderWidth: 3,
         flexDirection:'row',
-    
     },
-    
 
     gambiarra: {
         backgroundColor: '#FF800B',
